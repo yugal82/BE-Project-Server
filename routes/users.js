9f11422c597fdae7c17e5333c4dbbac8b6e12aea
Yugal Khanter <yugalkhanter82@gmail.com>
2024-02-26 18:41:03 +0530

    "/" route and controller added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getUsers, postUser, editInfo, getUserByAddress } = require('../controllers/usersController')
  
+router.get('/', async (req, res) => {
+    res.status(200).send("The server is live and responding well")
+});
 router.get('/users', getUsers);
 router.get('/user/:address', getUserByAddress);
 router.post('/signup', postUser);
